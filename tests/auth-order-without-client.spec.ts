2b3e8f3571f24b69612a850e3d509371c8563e9a
Ana-Gavriliuc <gavrilyuk-1994@mail.ru>
2025-11-17 16:11:56 +0200

    Fix formatting

--- tests/auth-order-without-client.spec.ts
+++ tests/auth-order-without-client.spec.ts
@@ -11,66 +11,59 @@ test(' Login and get order by id without api client', async ({ request }) => {
     data: LoginDTO.createLoginWithCorrectData(),
   })
   expect(authResponse.status()).toBe(StatusCodes.OK)
-  const jwt= await authResponse.text()
-
+  const jwt = await authResponse.text()
 
   const createOrderResponse = await request.post(`${BASE_URL}${ORDER_PATH}`, {
     data: OrderDTO.createOrderWithRandomData(),
     headers: {
       Authorization: `Bearer ${jwt}`,
-    }
+    },
   })
 
   const order = await createOrderResponse.json()
-  const id :number = order.id
+  const id: number = order.id
 
   const getOrderResponse = await request.get(`${BASE_URL}${ORDER_PATH}/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt}`,
-    }
+    },
   })
 
   expect(getOrderResponse.status()).toBe(StatusCodes.OK)
   const getOrder = await getOrderResponse.json()
   expect(getOrder.id).toBe(id)
-
 })
-
 
 test(' Login and delete order by id without api client', async ({ request }) => {
   const authResponse = await request.post(`${BASE_URL}/login/student`, {
     data: LoginDTO.createLoginWithCorrectData(),
   })
   expect(authResponse.status()).toBe(StatusCodes.OK)
-  const jwt= await authResponse.text()
-
+  const jwt = await authResponse.text()
 
   const createOrderResponse = await request.post(`${BASE_URL}${ORDER_PATH}`, {
     data: OrderDTO.createOrderWithRandomData(),
     headers: {
       Authorization: `Bearer ${jwt}`,
-    }
+    },
   })
 
   const order = await createOrderResponse.json()
-  const id :number = order.id
-
+  const id: number = order.id
 
   const deleteOrderResponse = await request.delete(`${BASE_URL}${ORDER_PATH}/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt}`,
-    }
+    },
   })
   expect(deleteOrderResponse.status()).toBe(StatusCodes.OK)
-
 
   const afterDeleteResponse = await request.get(`${BASE_URL}${ORDER_PATH}/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt}`,
-    }
+    },
   })
   expect(afterDeleteResponse.status()).toBe(StatusCodes.OK)
   const body = await afterDeleteResponse.text()
   expect(body).toBe('')
 })
-
